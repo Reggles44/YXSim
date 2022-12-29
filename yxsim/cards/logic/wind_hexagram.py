0b8651dcd3babb1ebef20d9e806a13caaefcd870
ilyas <ilyas.aricanli@gmail.com>
2022-12-29 17:23:06 -0500

    Update come cards, add five new cards

--- yxsim/cards/logic/wind_hexagram.py
+++ yxsim/cards/logic/wind_hexagram.py
@@ -1,28 +1,30 @@
 from yxsim.action import Action
 from yxsim.cards.base import Card
-from yxsim.combat import combat
 from yxsim.player import Player
-from yxsim.resources import Sect, Job, Resource
+from yxsim.resources import Sect, Resource
+from yxsim.combat import combat
+import typing
 
 
 class CardType(Card):
-    display_name = 'Divive Walk Fulu'
+    display_name = 'Wind Hexagram'
     phase = 1
-
-    job = Job.FULULIST
-    consumption = True
+    sect = Sect.HEPTASTAR
+    qi = 0
 
     def play(self, attacker: Player, defender: Player, **kwargs) -> Action:
-        self.exhausted = True
         return Action(
             card=self,
             source=attacker,
             target=attacker,
-            chase=True,
-            resource_changes={Resource.QI: 2}
+            resource_changes={Resource.QI: 1, Resource.HEXAGRAM: 1}
         ).execute()
 
     def test_card(self):
         card_user, opponent = self.generate_test_data()
         combat(card_user, opponent, limit=1)
-        assert card_user.resources[Resource.QI] == 2
+        assert card_user.resources[Resource.QI] == 1
+        assert card_user.resources[Resource.HEXAGRAM] == 1
+
+
+
